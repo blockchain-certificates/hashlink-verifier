d7f371cab6819106d6703c44e735ba5179937f1a
Julien Fraichot <julien.fraichot@hyland.com>
2022-07-06 14:53:31 -0500

    fix(Node): verify if variable is defined before reading it

--- src/hashlink/crypto.ts
+++ src/hashlink/crypto.ts
@@ -2,7 +2,7 @@
 import isomorphicCrypto from '@trust/webcrypto';
 let crypto;
 
-if (self.crypto || (self as any).msCrypto) {
+if (typeof self !== 'undefined' && (self.crypto || (self as any).msCrypto)) {
   crypto = self.crypto || (self as any).msCrypto;
 } else {
   crypto = isomorphicCrypto;
